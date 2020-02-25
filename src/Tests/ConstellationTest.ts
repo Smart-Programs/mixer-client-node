0d5fe540e6cfa5c2d88f89c23c9ffbb35331fc5a
Unsmart <me@unsmart.co>
2020-02-24 19:45:19 -0600

    update const

--- src/Tests/ConstellationTest.ts
+++ src/Tests/ConstellationTest.ts
@@ -1,7 +1,7 @@
 import { Client } from '../index'
 
 const client = new Client({
-	clientid: 'cdffa13a0b5535e44518c0076650cc12d0e464075f9194fb'
+    clientid: 'cdffa13a0b5535e44518c0076650cc12d0e464075f9194fb'
 })
 
 console.log('Try to subscribe to an unknown event... expect a not found error')
@@ -10,68 +10,84 @@ client.subscribeTo('channel:529479:fakeEvent') // should give us an error of not
 const constellation = client.constellationService
 
 constellation.on('subscribe', (data) => {
-	console.log(`We are now also subscribed to ${data.events.join(', ')}`)
+    console.log(`We are now also subscribed to ${data.events.join(', ')}`)
 
-	if (data.events.toString() === 'channel:529479:update') {
-		client.subscribeTo([ 'channel:529479:followed', 'channel:529479:hosted' ])
-	} else {
-		if (constellation.subscribedEvents.length === 3) {
-			console.log('We successfully subscribed to all 3 events correctly...')
-			console.log(client.subscribedEvents.join(', '))
+    if (data.events.toString() === 'channel:529479:update') {
+        client.subscribeTo(['channel:529479:followed', 'channel:529479:hosted'])
+    } else if (
+        data.events.join(', ') ===
+        'channel:529479:followed, channel:529479:hosted'
+    ) {
+        client.subscribeTo(['channel:60302:followed', 'channel:60302:hosted'])
+    } else {
+        if (constellation.subscribedEvents.length === 5) {
+            console.log(
+                'We successfully subscribed to all 5 events correctly...'
+            )
+            console.log(client.subscribedEvents.join(', '))
 
-			console.log('Attempting to unsubscribe from all events...')
+            console.log('Attempting to unsubscribe from all events...')
 
-			client.unsubscribeTo(client.subscribedEvents)
-		} else {
-			console.error('We did not subscribe to all 3 events correctly...')
-			console.error(client.subscribedEvents.join(', '))
+            client.unsubscribeTo(client.subscribedEvents)
+        } else {
+            console.error('We did not subscribe to all 5 events correctly...')
+            console.error(client.subscribedEvents.join(', '))
 
-			process.exit(1)
-		}
-	}
+            process.exit(1)
+        }
+    }
 })
 
+let unsubCount = 0
+let to
 constellation.on('unsubscribe', (data) => {
-	if (data.events.length === 3 && constellation.subscribedEvents.length === 0) {
-		console.log('We successfully unsubscribed to all 3 events correctly...')
+    if (to) clearTimeout(to)
 
-		console.log('All test were completed successfully...')
-		process.exit(0)
-	} else {
-		console.error('We did not unsubscribe to all 3 events correctly...')
-		process.exit(1)
-	}
+    unsubCount += data.events.length
+
+    if (unsubCount === 5 && constellation.subscribedEvents.length === 0) {
+        console.log('We successfully unsubscribed to all 5 events correctly...')
+
+        console.log('All test were completed successfully...')
+        process.exit(0)
+    } else {
+        to = setTimeout(() => {
+            console.error('We did not unsubscribe to all 5 events correctly...')
+            process.exit(1)
+        }, 3000)
+    }
 })
 
 constellation.on('event', (data, event) => {
-	if (data.event === 'hello') console.log('The constellation socket is now connected...')
-	else console.log(JSON.stringify(data), event)
+    if (data.event === 'hello')
+        console.log('The constellation socket is now connected...')
+    else console.log(JSON.stringify(data), event)
 })
 
 constellation.on('error', (data) => {
-	console.error('An unexpected error occurred at constellation.on error')
-	console.error(data)
+    console.error('An unexpected error occurred at constellation.on error')
+    console.error(data)
 
-	process.exit(1)
+    process.exit(1)
 })
 
 constellation.on('warning', (data) => {
-	if (data.code === 2001 && data.id === 1) {
-		console.log('An expected warning occurred... (Duplicate Subscribe)')
-	} else {
-		console.warn('An unexpected error occurred at constellation.on error')
-		console.warn(data)
+    if (data.code === 2001 && data.id === 1) {
+        console.log('An expected warning occurred... (Duplicate Subscribe)')
+    } else {
+        console.warn('An unexpected error occurred at constellation.on error')
+        console.warn(data)
 
-		process.exit(1)
-	}
+        process.exit(1)
+    }
 })
 
 constellation.on('reply', (data) => {
-	if (data.error && data.error.code === 4106) {
-		console.log('An expected error occurred... (Not Found Event)')
-		client.subscribeTo('channel:529479:update')
-		client.subscribeTo('channel:529479:update')
-	} else {
-		console.log(data)
-	}
+    if (data.error && data.error.code === 4106) {
+        console.log('An expected error occurred... (Not Found Event)')
+        client.subscribeTo('channel:529479:update')
+        client.subscribeTo('channel:529479:update')
+    } else {
+        console.log(data)
+    }
 })
