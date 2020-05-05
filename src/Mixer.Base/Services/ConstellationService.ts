d6e948458fb0b58c6a4e0e3d4b22d9a6b0f796d0
Unsmart <me@unsmart.co>
2020-05-05 12:30:01 -0500

    reverts to constellation and add in WS fallback

--- src/Mixer.Base/Services/ConstellationService.ts
+++ src/Mixer.Base/Services/ConstellationService.ts
@@ -1,289 +1,220 @@
 import { EventEmitter } from 'events'
-import * as WebSocket from 'ws'
 import { toJSON } from '../Util/HelperFunctions'
 
-class ConstellationService extends EventEmitter {
-    private sockets: Map<number, WebSocket> = new Map()
-    private events: Map<number, string[]> = new Map()
-    private subscribingTo: Map<number, { number?: string[] }> = new Map()
-    private unsubscribingTo: Map<number, { number?: string[] }> = new Map()
-    private currentId: Map<number, number> = new Map()
-    private subNumber = 0
-    private splitCount = 2000
+let WebSocket
 
-    constructor (private clientid: string) {
+if (typeof window === 'undefined') WebSocket = require('ws')
+else WebSocket = window.WebSocket
+
+class ConstellationService extends EventEmitter {
+    private socket: WebSocket
+    private events: string[] = []
+    private subscribingTo: { number?: string[] } = {}
+    private unsubscribingTo: { number?: string[] } = {}
+    private currentId = 0
+
+    constructor(private clientid: string) {
         super()
 
-        this.createSocket(0, 'constructor')
+        this.createSocket()
     }
 
     /*
      * Create the constellation socket
      */
-    private createSocket (number: number, from?) {
-        // console.log('Create Socket.', number, 'From:', from)
-        this.currentId.set(number, 0)
-        this.subscribingTo.set(number, {})
-        this.unsubscribingTo.set(number, {})
+    private createSocket() {
+        this.shouldClose = false
+        if (this.socket && this.socket.readyState === 1) return
 
-        if (
-            number &&
-            this.sockets.has(number) &&
-            this.sockets.get(number).readyState === 1
-        )
-            this.sockets.get(number).terminate()
+        this.currentId = 0
 
-        const num = number ? number : this.sockets.values.length
-
-        this.sockets.set(
-            num,
-            new WebSocket('wss://constellation.mixer.com', 'cnstl-gzip', {
+        this.socket = new WebSocket(
+            'wss://constellation.mixer.com',
+            'cnstl-gzip',
+            {
                 headers: {
                     'client-id': this.clientid,
-                    'x-is-bot': true
-                } as any
-            })
+                    'x-is-bot': true,
+                    'User-Agent': 'mixer-client-node / I dont do versions tbh',
+                } as any,
+            }
         )
 
-        this.eventListener(num)
+        this.eventListener()
 
-        if (this.events.has(num) && this.events.get(num).length > 0) {
-            const subTo = this.events.get(num)
-            this.events.set(num, [])
+        if (this.events.length > 0) {
+            const subTo = this.events
+            this.events = []
 
-            this.subscribe(subTo, num)
+            this.subscribe(subTo)
+        }
+    }
+
+    public start() {
+        this.createSocket()
+    }
+
+    private shouldClose = false
+    public stop() {
+        if (this.socket) {
+            this.shouldClose = true
+            this.socket.close()
         }
     }
 
     /*
      * Ping the chat to fix disconnect issues
      */
-    private timeout: Map<number, NodeJS.Timeout> = new Map()
-    private pingId: Map<number, number> = new Map()
-    private ping (num: number) {
-        // Send ping every 5 seconds to ensure the connection is solid
-        setTimeout(() => this.sendPing(num), 1000 * 5)
+    private timeout: NodeJS.Timeout
+    private pingId = 0
+    private ping() {
+        // Send ping every 5 minutes to ensure the connection is solid
+        setTimeout(() => this.sendPing, 1000 * 5)
 
-        if (this.timeout.get(num)) clearTimeout(this.timeout.get(num))
+        if (this.timeout) clearTimeout(this.timeout)
+        // If the ping does not get responded too in 1 second restart the socket
+        this.timeout = setTimeout(() => this.createSocket, 1000)
     }
 
-    private sendPing (num: number) {
-        if (this.sockets.has(num) && this.sockets.get(num).readyState !== 1)
-            return
-        if (this.currentId.get(num) > 100000000) this.currentId.set(num, 0)
-        this.pingId.set(num, this.currentId.get(num) + 1)
-        this.currentId.set(num, this.pingId.get(num))
-        this.sendPacket('ping', null, this.pingId.get(num), num)
-
-        // If the ping does not get responded too in 1 second restart the socket
-        this.timeout.set(
-            num,
-            setTimeout(() => this.createSocket(num, 'pingTimeout'), 1000)
-        )
+    private sendPing() {
+        if (this.socket.readyState !== 1) return
+        if (this.currentId > 100000000) this.currentId = 0
+        this.pingId = ++this.currentId
+        this.sendPacket('ping', null, this.pingId)
     }
 
     /*
      * Get the event's that you are subscribed to
      * Returns a string array
      */
-    public get subscribedEvents (): string[] {
-        let subscribed = []
-        this.events.forEach((items) => {
-            subscribed = [...subscribed, ...items]
-        })
-        return subscribed
+    public get subscribedEvents(): string[] {
+        return [...this.events]
     }
 
     /*
      * Setup the socket event listener to emit events
      */
-    private eventListener (number: number) {
-        this.sockets.get(number).once('open', () => this.ping(number))
+    private eventListener() {
+        this.socket.addEventListener('open', this.ping)
 
-        this.sockets
-            .get(number)
-            .addEventListener('error', ({ error, message }) => {
-                if (this.listenerCount('error') > 0)
-                    this.emit('error', { error, message })
-            })
+        this.socket.addEventListener('error', (e) => {
+            if (this.listenerCount('error') > 0) this.emit('error', e)
+        })
 
-        this.sockets
-            .get(number)
-            .addEventListener('close', () =>
-                setTimeout(() => this.createSocket(number, 'socketClose'), 500)
-            )
+        this.socket.addEventListener('close', () => {
+            if (this.shouldClose === false)
+                setTimeout(() => this.createSocket(), 500)
+        })
 
-        this.sockets
-            .get(number)
-            .addEventListener('message', ({ data: response }) => {
-                const data: {
-                    data: any
-                    event?: string
-                    type: 'reply' | 'event'
-                    id: number
-                    error?: any
-                    result?: any
-                } = toJSON(response) as any
+        this.socket.addEventListener('message', ({ data: response }) => {
+            const data: {
+                data: any
+                event?: string
+                type: 'reply' | 'event'
+                id: number
+                error?: any
+                result?: any
+            } = toJSON(response) as any
 
-                if (data.event === 'hello') return this.emit('open', data.data)
+            if (data.event === 'hello') return this.emit('open', data.data)
 
-                if (data.type === 'reply') {
-                    if (this.subscribingTo.get(number)[data.id]) {
-                        const events = this.subscribingTo.get(number)[data.id]
-
-                        const newEvents = this.subscribingTo.get(number)
-                        delete newEvents[data.id]
-                        this.subscribingTo.set(number, newEvents)
-
-                        if (data.error)
-                            this.emit(data.type, {
-                                result: data.result,
-                                error: data.error,
-                                events
-                            })
-                        else {
-                            // subscribe success
-                            const old = this.events.has(number)
-                                ? this.events.get(number)
-                                : []
-                            this.events.set(number, [...old, ...events])
-                            this.emit('subscribe', { events })
-                        }
-                    } else if (this.unsubscribingTo.get(number)[data.id]) {
-                        const events = this.unsubscribingTo.get(number)[data.id]
-
-                        const newEvents = this.unsubscribingTo.get(number)
-                        delete newEvents[data.id]
-                        this.unsubscribingTo.set(number, newEvents)
-
-                        if (data.error)
-                            this.emit(data.type, {
-                                result: data.result,
-                                error: data.error,
-                                events
-                            })
-                        else {
-                            // unsubscribe success
-                            this.events.set(
-                                number,
-                                this.events
-                                    .get(number)
-                                    .filter((event) => !events.includes(event))
-                            )
-                            this.emit('unsubscribe', { events })
-                        }
-                    } else if (data.id === this.pingId.get(number)) {
-                        if (data.error) this.createSocket(number, 'pingError')
-                        else this.ping(number)
-                    } else this.emit(data.type, data)
-                } else if (data.data && data.data.payload) {
-                    this.emit(data.type, data.data.payload, data.data.channel)
-                } else {
-                    this.emit(data.type, data)
-                }
-            })
+            if (data.type === 'reply') {
+                if (data.id in this.subscribingTo) {
+                    const events = this.subscribingTo[data.id]
+                    delete this.subscribingTo[data.id]
+                    if (data.error)
+                        this.emit(data.type, {
+                            result: data.result,
+                            error: data.error,
+                            events,
+                        })
+                    else {
+                        // subscribe success
+                        this.events = [...this.events, ...events]
+                        this.emit('subscribe', { events })
+                    }
+                } else if (data.id in this.unsubscribingTo) {
+                    const events = this.unsubscribingTo[data.id]
+                    delete this.unsubscribingTo[data.id]
+                    if (data.error)
+                        this.emit(data.type, {
+                            result: data.result,
+                            error: data.error,
+                            events,
+                        })
+                    else {
+                        // unsubscribe success
+                        this.events = this.events.filter(
+                            (event) => !events.includes(event)
+                        )
+                        this.emit('unsubscribe', { events })
+                    }
+                } else if (data.id === this.pingId) {
+                    if (data.error) this.createSocket()
+                    else this.ping()
+                } else this.emit(data.type, data)
+            } else if (data.data && data.data.payload) {
+                this.emit(data.type, data.data.payload, data.data.channel)
+            } else {
+                this.emit(data.type, data)
+            }
+        })
     }
 
     /*
      * Send the socket data
      */
-    private sendPacket (
-        method: string,
-        params: IParams,
-        id: number = 0,
-        num: number
-    ) {
+    private sendPacket(method: string, params: IParams, id: number = 0) {
         const packet: IPacket = {
             id,
             method,
             params,
-            type: 'method'
+            type: 'method',
         }
-        if (this.sockets.has(num) && this.sockets.get(num).readyState === 1) {
-            this.sockets.get(num).send(JSON.stringify(packet))
-        } else {
+        if (this.socket && this.socket.readyState === 1)
+            this.socket.send(JSON.stringify(packet))
+        else {
             this.emit('warning', {
                 code: 2000,
                 events: params,
                 id: 1,
                 method,
                 reason: 'Socket Closed or No Socket Found',
-                warning: "Can't Send Packet"
+                warning: "Can't Send Packet",
             })
         }
-    }
-
-    private resetSubNumber () {
-        this.events.forEach((val, key) => {
-            if (this.subNumber < key) return
-            else if (val.length < this.splitCount) this.subNumber = key
-        })
     }
 
     /*
      * Subscribe to an event
      * Emits a subscribe event if successful
      */
-    public subscribe (event: string | string[], num?: number) {
-        const number = num ? num : this.subNumber
-
-        if (this.sockets.get(number).readyState !== 1) {
+    public subscribe(event: string | string[]) {
+        if (this.socket.readyState !== 1) {
             this.once('open', () => {
-                this.subscribe(event, num)
+                this.subscribe(event)
             })
         } else {
             event = typeof event === 'string' ? [event] : event
             const originalEvents = event
             event = event.filter((name) => {
-                for (const event in this.events) {
-                    if (event.includes(name)) return false
+                if (this.events.includes(name)) return false
+                else {
+                    for (const id in this.subscribingTo) {
+                        if (
+                            this.subscribingTo.hasOwnProperty(id) &&
+                            this.subscribingTo[id].includes(name)
+                        )
+                            return false
+                    }
+                    return true
                 }
-
-                for (const id in this.subscribingTo) {
-                    if (
-                        this.subscribingTo.hasOwnProperty(id) &&
-                        this.subscribingTo[id].includes(name)
-                    )
-                        return false
-                }
-
-                return true
             })
 
             if (event.length > 0) {
-                let subbingToCount = 0
-
-                for (const i in this.subscribingTo.get(number)) {
-                    subbingToCount += this.subscribingTo.get(number)[i].length
-                }
-
-                const c = this.events.has(number)
-                    ? this.events.get(number).length +
-                      event.length +
-                      subbingToCount
-                    : event.length + subbingToCount
-
-                if (!num && c >= this.splitCount) {
-                    this.createSocket(++this.subNumber, 'subCountHigher')
-                }
-
-                const threshold = Math.floor(
-                    (this.subNumber - 1) * this.splitCount * 0.6
-                )
-                if (
-                    this.subNumber > 10 &&
-                    this.subscribedEvents.length < threshold
-                )
-                    this.resetSubNumber()
-
-                const id = this.currentId.get(number) + 1
-                this.currentId.set(number, id)
-
-                const newEvents = this.subscribingTo.get(number)
-                newEvents[id] = event
-                this.subscribingTo.set(number, newEvents)
-
-                this.sendPacket('livesubscribe', { events: event }, id, number)
+                const id = ++this.currentId
+                this.subscribingTo[id] = event
+                this.sendPacket('livesubscribe', { events: event }, id)
             } else {
                 this.emit('warning', {
                     code: 2001,
@@ -291,7 +222,7 @@ class ConstellationService extends EventEmitter {
                     id: 1,
                     reason:
                         'You are already subscribed to the event(s) you said to subscribe to',
-                    warning: "Can't Subscribe"
+                    warning: "Can't Subscribe",
                 })
             }
         }
@@ -301,48 +232,32 @@ class ConstellationService extends EventEmitter {
      * UbSubscribe to an event
      * Emits an unsubscribe event if successful
      */
-    public unsubscribe (event: string | string[]) {
-        const events: Map<number, string[]> = new Map()
-        let hasSome = false
+    public unsubscribe(event: string | string[]) {
         event = typeof event === 'string' ? [event] : event
-        event = event.filter((name) => {
-            this.events.forEach((values, num) => {
-                values.forEach((val) => {
-                    if (name === val) {
-                        const old = events.get(num) || []
-                        events.set(num, [...old, val])
-                        hasSome = true
-                    }
-                })
+        event = event.filter((name) => this.events.includes(name))
+
+        if (event.length > 0 && this.socket.readyState !== 1) {
+            this.createSocket()
+            this.emit('error', {
+                code: 404,
+                events: event,
+                id: 1,
+                reason: 'The socket was closed',
+                warning: "Can't Send Packet",
             })
-        })
-
-        this.events.forEach((val, key) => {
-            if (this.sockets.get(key).readyState !== 1) {
-                this.createSocket(key, 'socketNotReadyUnsub')
-            }
-        })
-
-        if (!hasSome) {
+        } else if (event.length === 0) {
             this.emit('warning', {
                 code: 2000,
                 events: event,
                 id: 2,
                 reason:
                     'You are not subscribed to any of the events you listed to unsubscribe to',
-                warning: "Can't Send Packet"
+                warning: "Can't Send Packet",
             })
         } else {
-            events.forEach((items, num) => {
-                const id = this.currentId.get(num) + 1
-                this.currentId.set(num, id)
-
-                const newEvents = this.unsubscribingTo.get(num)
-                newEvents[id] = items
-                this.unsubscribingTo.set(num, newEvents)
-
-                this.sendPacket('liveunsubscribe', { events: items }, id, num)
-            })
+            const id = ++this.currentId
+            this.unsubscribingTo[id] = event
+            this.sendPacket('liveunsubscribe', { events: event }, id)
         }
     }
 }
