e9c9c4dc79616798e9293ce32f12ef6db15ba68d
Garret <me@unsmart.co>
2020-05-07 12:27:57 -0500

    Update ChatService.ts

--- src/Mixer.Base/Services/ChatService.ts
+++ src/Mixer.Base/Services/ChatService.ts
@@ -49,7 +49,7 @@ class ChatService extends EventEmitter {
         })
     }
     
-    public joinWithAuthKey({userid, channelid, authKey, endpoints, autoReconnect = true} = {}) {
+    public joinWithAuthKey({userid, channelid, authKey, endpoints, autoReconnect = true}) {
         if (!this.client.user || userid !== this.client.user.userid) {
             let id: number
             if (this.client.user) id = this.client.user.channelid
